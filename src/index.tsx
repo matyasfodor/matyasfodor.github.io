2a01115b90f326a4b8ef7377a248cb22998d5fc2
Mátyás Fodor <matyas.fodor@gmail.com>
2020-12-25 23:20:50 +0100

    feat: add GA

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import ReactGA from "react-ga";
 
 import App from "./App";
 import "./index.scss";
+
+ReactGA.initialize("G-230YFEX4MF");
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 ReactDOM.render(
   <React.StrictMode>
