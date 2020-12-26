a92f96381248604f07155a113d5e8bb3421e7850
Mátyás Fodor <matyas.fodor@gmail.com>
2020-12-26 08:29:33 +0100

    feat: fix GA?

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,8 @@ import ReactGA from "react-ga";
 import App from "./App";
 import "./index.scss";
 
-ReactGA.initialize("G-230YFEX4MF");
+ReactGA.initialize("UA-186090362-1");
+// ReactGA.initialize("G-230YFEX4MF");
 ReactGA.pageview(window.location.pathname + window.location.search);
 
 ReactDOM.render(
