1b8667068908f0e470cefcf35bae482a34673107
Mátyás Fodor <matyas.fodor@gmail.com>
2021-03-09 20:03:49 +0100

    feat: wording

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import MetaTags from "react-meta-tags";
 import useDarkMode from "use-dark-mode";
 
 import Toggle from "./Toggle";
+import MetaTags from "./MetaTags";
 import SocialLinks from "./SocialLinks";
 
 import "./App.scss";
@@ -10,21 +10,7 @@ function App() {
   const darkMode = useDarkMode();
   return (
     <div className="App">
-      <MetaTags>
-        <title>Matyas Fodor Software Engineer</title>
-        <meta
-          name="description"
-          content="Personal website of Matyas Fodor, a fullstack software engineer proficient in Python, TypeScript and JavaScript."
-        />
-        <meta property="og:title" content="Matyas Fodor" />
-        {/* TODO add image */}
-        {/* <meta property="og:image" content="path/to/image.jpg" /> */}
-        <meta name="robots" content="index, follow" />
-        <meta
-          name="keywords"
-          content="software engineer,software developer,algorithms,python,typescript,javascript,react,rust,nextjs,prisma,graphql"
-        />
-      </MetaTags>
+      <MetaTags />
       <header>
         <div className="toggleContainer">
           <Toggle value={darkMode.value} onChange={darkMode.toggle} />
@@ -33,22 +19,21 @@ function App() {
       </header>
       <section className="mainSection">
         <p>
-          I am an AI graduate currently working as a fullstack software engineer
-          and tech lead at{" "}
+          I am an AI graduate currently working as a fullstack developer and
+          tech lead at{" "}
           <a rel="noreferrer noopener" href="https://www.benevolent.com/">
             BenevolentAI
           </a>
           .
         </p>
         <p>
-          In my free time I enjoy cracking coding puzzles, visiting bakeries for
-          overpriced ☕️ and 🥐.
+          In my free time, I am tinkering with neural nets 🕸 and Rust 🦀 or
+          visiting bakeries for overpriced ☕️ and 🥐.
         </p>
         <p>
-          I am an endurance sports and functional training enthusiast, I
-          finished an iron man in 2018. 🏊‍♂️ 🚴‍♂️ 🏃‍♂️
+          I am an endurance sports and functional training enthusiast, in 2018 I
+          finished an iron man. 🏊‍♂️ 🚴‍♂️ 🏃‍♂️
         </p>
-        <p>In my free time I am tinkering with neural nets 🕸 and Rust 🦀.</p>
       </section>
       <footer>
         <SocialLinks />
