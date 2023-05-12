6718155d4594ddde7420d67a2f1622050f3dde14
Matyas Fodor <matyas.fodor@gmail.com>
2023-05-12 13:37:45 +0200

    feat: use GA4

--- src/hooks/useGA.tsx
+++ src/hooks/useGA.tsx
@@ -1,18 +1,17 @@
 import { useRouter } from "next/router";
 import { useEffect } from "react";
-import ReactGA from "react-ga";
+import ReactGA from 'react-ga4';
 
 export function useGA(GAId: string) {
   const router = useRouter();
 
   useEffect(() => {
     const handleRouteChange = (url: string) => {
-      ReactGA.set({ page: url });
-      ReactGA.pageview(url);
+      ReactGA.send({hitType: "pageview", page: url})
     };
-    ReactGA.initialize(GAId, { debug: false });
-    ReactGA.set({ page: router.pathname });
-    ReactGA.pageview(router.pathname);
+    ReactGA.initialize(GAId, { testMode: false });
+    ReactGA.send({hitType: "pageview", page: router.pathname})
+
     router.events.on("routeChangeComplete", handleRouteChange);
     return () => {
       router.events.off("routeChangeComplete", handleRouteChange);
